b4a90529971652ff4e80b065857f0fb999e7e9e7
Data-drone <bpl.law@gmail.com>
2022-03-27 01:45:44 +1100

    adding new trigger code to db_notebook

--- db_trigger_notebook.py
+++ db_trigger_notebook.py
@@ -20,4 +20,12 @@
 
 # COMMAND ----------
 
+from sparkdl import HorovodRunner
+import horovod.torch as hvd
+from fashion_mnist_main import main_hvd
 
+hr = HorovodRunner(np=1, driver_log_verbosity='all')
+
+model = hr.run(main_hvd)
+
+# COMMAND ----------
